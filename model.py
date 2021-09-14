cd5171ac89b4bdd89e993ecdbebf0393a8f1a147
Sai Sasank <sai.sasank.yadati@gmail.com>
2021-09-14 10:04:32 +0530

    simplify

--- model.py
+++ model.py
@@ -4,29 +4,30 @@
 # imports
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-import keras
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten, Activation
-from keras.layers import Conv2D, MaxPooling2D
-from keras.optimizers import SGD
-from keras.datasets import cifar10
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten, Activation
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.datasets import mnist
 from keras.utils import np_utils
-from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from keras.constraints import maxnorm
+import numpy as np
 
 # hyperparameters
 BATCH_SIZE = 32
 NUM_CLASSES = 10
-NUM_EPOCHS = 30
+LR = 0.01
+NUM_EPOCHS = 5
 
 def load_data(num_classes):
     '''
-    load cifar10 data
+    load mnist data
     scale it to (0,1)
     return train, test
     '''
     # get data
-    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
     
     # one hot vectors
     y_train = np_utils.to_categorical(y_train, num_classes)
@@ -35,6 +36,9 @@ def load_data(num_classes):
     # cast it float32
     x_train = x_train.astype('float32')
     x_test = x_test.astype('float32')
+
+    x_train = np.expand_dims(x_train, -1)
+    x_test = np.expand_dims(x_test, -1)
     
     # scale pixels to (0,1)
     x_train /= 255.0
@@ -42,7 +46,7 @@ def load_data(num_classes):
 
     return x_train, y_train, x_test, y_test
 
-def create_model(num_classes):
+def create_model(num_classes, activation='relu'):
     '''
     construct and compile model.
     return model.
@@ -50,41 +54,23 @@ def create_model(num_classes):
     # Create the model
     model = Sequential()
 
-    model.add(Conv2D(32, (3, 3), input_shape=(32, 32, 3), activation='relu', padding='same'))
-    model.add(Dropout(0.2))
-    
-    model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+    model.add(tf.keras.Input(shape=(28, 28, 1)))
+    model.add(Conv2D(32, (3, 3), activation=activation, padding='same'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     
-    model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
-    model.add(Dropout(0.2))
-    
-    model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(64, (3, 3), activation=activation, padding='same'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    
-    model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
-    model.add(Dropout(0.2))
-    
-    model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    
+
     model.add(Flatten())
-    model.add(Dropout(0.2))
-    
-    model.add(Dense(1024, activation='relu', kernel_constraint=maxnorm(3)))
-    model.add(Dropout(0.2))
-    
-    model.add(Dense(512, activation='relu', kernel_constraint=maxnorm(3)))
-    model.add(Dropout(0.2))
+
+    model.add(Dense(128, activation=activation))
+    model.add(Dropout(0.5))
     
     model.add(Dense(num_classes, activation='softmax'))
     
     # Compile model
-    epochs = 30
-    lrate = 0.01
-    decay = lrate/epochs
-    sgd = SGD(lr=lrate, momentum=0.9, decay=decay, nesterov=False)
-    model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+    opt = tf.keras.optimizers.Adam()
+    model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
     return model
 
 def train(model, x_train, y_train, x_test, y_test, num_epochs, batch_size):
@@ -92,8 +78,7 @@ def train(model, x_train, y_train, x_test, y_test, num_epochs, batch_size):
     train model.
     returns model.
     '''
-    model.fit(x_train, y_train, batch_size=batch_size, epochs=num_epochs, validation_data=(x_test, y_test), shuffle=True, verbose=2)
-    
+    model.fit(x_train, y_train, batch_size=batch_size, epochs=num_epochs, validation_split=0.1, shuffle=True)    
     return model
 
     
@@ -101,9 +86,9 @@ if __name__ == '__main__':
     # get data
     x_train, y_train, x_test, y_test = load_data(NUM_CLASSES)
     # create model
-    model = create_model(NUM_CLASSES)
+    model_with_relu = create_model(NUM_CLASSES)
     # train model
-    model = train(model, x_train, y_train, x_test, y_test, NUM_EPOCHS, BATCH_SIZE)
+    model_with_relu = train(model_with_relu, x_train, y_train, x_test, y_test, NUM_EPOCHS, BATCH_SIZE)
     # save model
-    model.save('model_cifar10.h5')
+    model_with_relu.save('model_relu.h5')
     
